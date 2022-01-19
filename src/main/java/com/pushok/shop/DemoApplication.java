80512da3ca1964535b604aec8ca7ef533de3a046
kolquethas <kolquethas@gmail.com>
2022-01-19 14:21:44 +0300

    макет2

--- src/main/java/com/pushok/shop/DemoApplication.java
+++ src/main/java/com/pushok/shop/DemoApplication.java
@@ -2,8 +2,16 @@ package com.pushok.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@ComponentScan("com")
 public class DemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/pushok/shop/ServletInitializer.java
+++ src/main/java/com/pushok/shop/ServletInitializer.java
@@ -3,6 +3,9 @@ package com.pushok.shop;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+
+
+
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
